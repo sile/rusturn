03d15b4b30cefc3b2378183736fd22599ddb4530
Takeru Ohta <phjgt308@gmail.com>
2018-09-11 00:17:55 +0900

    Update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,14 @@
-#[macro_use]
-extern crate slog;
 extern crate fibers;
 extern crate futures;
 extern crate rand;
 extern crate rustun;
+extern crate stun_codec;
 #[macro_use]
 extern crate trackable;
-extern crate handy_async;
 
-macro_rules! track_try {
-    ($expr:expr) => {
-        track!($expr.map_err(::Error::from))?
-    };
-}
-macro_rules! track_err {
-    ($expr:expr) => {
-        $expr.map_err(|e| track!(::Error::from(e)))
-    };
-}
+pub use rustun::{Error, ErrorKind, Result};
 
-pub use attribute::Attribute;
-pub use method::Method;
-pub use rustun::{Error, ErrorKind};
-
-pub mod rfc5766;
-pub mod server;
+//pub mod server;
 pub mod types;
 
-mod attribute;
-mod method;
-
-pub type Result<T> = ::std::result::Result<T, Error>;
-
-type BoxFuture<T, E> = Box<futures::Future<Item = T, Error = E> + Send + 'static>;
+pub const DEFUALT_LIFETIME_SECONDS: u64 = 10 * 60;
--- src/rfc5766/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod attributes;
-pub mod errors;
-pub mod methods;
