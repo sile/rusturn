12512c9f8ff848f94d3fc1f763373c04e9bcc80d
Takeru Ohta <phjgt308@gmail.com>
2018-09-17 23:41:25 +0900

    Refactor `transport` module

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -4,24 +4,25 @@ use rustun;
 use attribute::Attribute;
 use turn_message::{TurnMessageDecoder, TurnMessageEncoder};
 
-pub use self::channel_data::ChannelDataTransporter;
-pub use self::stun::StunTransporter;
+pub(crate) use self::channel_data::ChannelDataTransporter;
+pub(crate) use self::stun::StunTransporter;
+
 pub use self::udp_over_turn::UdpOverTurnTransporter;
 
 mod channel_data;
 mod stun;
 mod udp_over_turn;
 
-pub type StunTcpTransporter =
+pub(crate) type StunTcpTransporter =
     rustun::transport::StunTcpTransporter<StunTransporter<TurnTcpTransporter>>;
 
-pub type StunUdpTransporter =
+pub(crate) type StunUdpTransporter =
     rustun::transport::StunUdpTransporter<Attribute, StunTransporter<TurnUdpTransporter>>;
 
-pub type ChannelDataTcpTransporter = ChannelDataTransporter<TurnTcpTransporter>;
+pub(crate) type ChannelDataTcpTransporter = ChannelDataTransporter<TurnTcpTransporter>;
 
-pub type ChannelDataUdpTransporter = ChannelDataTransporter<TurnUdpTransporter>;
+pub(crate) type ChannelDataUdpTransporter = ChannelDataTransporter<TurnUdpTransporter>;
 
-pub type TurnUdpTransporter = UdpTransporter<TurnMessageEncoder, TurnMessageDecoder>;
+pub(crate) type TurnUdpTransporter = UdpTransporter<TurnMessageEncoder, TurnMessageDecoder>;
 
-pub type TurnTcpTransporter = TcpTransporter<TurnMessageEncoder, TurnMessageDecoder>;
+pub(crate) type TurnTcpTransporter = TcpTransporter<TurnMessageEncoder, TurnMessageDecoder>;
