aea400c8583b2a4a1fa421837cb96ac6c81960d3
Takeru Ohta <phjgt308@gmail.com>
2022-03-19 12:19:12 +0900

    Add logger

--- examples/turnsrv.rs
+++ examples/turnsrv.rs
@@ -31,6 +31,10 @@ struct Opt {
 }
 
 fn main() -> Result<(), trackable::error::MainError> {
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
+
     let opt = Opt::parse();
 
     let server_addr = opt.server;
